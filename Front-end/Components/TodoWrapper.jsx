2d1bfe3c56bb28aefd0bffbf10e51cbef96a8089
Pedro De Campos Faria <pedroafonso939@yahoo.com.br>
2024-06-09 22:38:00 -0300

    Implementando As classes todoform, todolist, todowrapper e criando o todoWrapperservice

--- Front-end/Components/TodoWrapper.jsx
+++ Front-end/Components/TodoWrapper.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import {TodoForm} from "./TodoForm";
-import {TodoList} from "./Todolist";
-
+import React, { useState } from 'react';
+import { TodoForm } from './TodoForm';
+import { TodoList } from './TodoList';
+import { v4 as uuidv4 } from "uuid";
 export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([
+        {id: 1, description: 'Tarefa exemplo', completed: false}
+    ]);
+    const addTodo = (todo) => {
+        setTodos([
+            ...todos,
+            { id: uuidv4(), task: description, completed: false },
+        ]);
+    }
     return (
         <div className='TodoWrapper'>
             <h1>Lista de Tarefas</h1>
-            <TodoForm/>
-            <TodoList/>
+            <TodoForm addTodo={addTodo}/>
+            {todos.map((item) =>
+                <TodoList
+                    key={item.id}
+                    task={item}
+                />
+            )
+            }
         </div>
     );
 }
--- Front-end/Components/Todolist.jsx
+++ Front-end/Components/Todolist.jsx
@@ -1,20 +1,9 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-export const TodoList = () => {
+import React from 'react'
+export const TodoList = ({task}) => {
     return (
-        <div className='Todo'>
-            <p
-                className='completed'
-            >
-                Tarefa 1
-            </p>
-            <div>
-                <FontAwesomeIcon
-                    className='delete-icon'
-                    icon={faTrash}
-                />
-            </div>
+        <div className="Todo">
+            <p className={`${task.completed ? "completed" : "incompleted"}`}
+            >{task.description}</p>
         </div>
-    );
-};
+    )
+}
